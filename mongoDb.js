f5313292adf23c7e2d6e9db75df6221074a2cfcb
Rizwan Ali Khan <rizwanalikhan6042@gmail.com>
2024-02-13 22:53:08 +0530

    Update mongoDb.js

--- mongoDb.js
+++ mongoDb.js
@@ -2,39 +2,39 @@
 const getData=require('./mongo1Db');
 
 
-// async function getData (){
-// let result=await client.connect();
-// let db=result.db(dataBase);
-// let collection=db.collection('products');
-// let response = await collection.find({}).toArray();
-// console.log(response);
-// }
-// //node ./mongoDb.js
-// getData();
-// async function getData (){
-//     let result=await client.connect();
-//     let db=result.db(dataBase);
-//     return db.collection('products');
-//     let response = await collection.find({name:"rizwan Ali Khan"}).toArray();
-//     console.log(response);
-//     }
+async function getData (){
+let result=await client.connect();
+let db=result.db(dataBase);
+let collection=db.collection('products');
+let response = await collection.find({}).toArray();
+console.log(response);
+}
+//node ./mongoDb.js
+getData();
+async function getData (){
+    let result=await client.connect();
+    let db=result.db(dataBase);
+    return db.collection('products');
+    let response = await collection.find({name:"rizwan Ali Khan"}).toArray();
+    console.log(response);
+    }
     // If you want to search any particular database you can add it into curly brackets like above
    
-    //Now we are creating separate functions for finding
+    // Now we are creating separate functions for finding
 
-    // async function getData (){
-    //     let result=await client.connect();
-    //     let db=result.db(dataBase);
-    //     return db.collection('products');
+    async function getData (){
+        let result=await client.connect();
+        let db=result.db(dataBase);
+        return db.collection('products');
        
-    //     }
-        //By promises then
-    // getData().then((resp)=>{
-    // resp.find({}).toArray().then((data)=>{
-    //     console.log(data);
-    // })
-    // })
-    // console.log(getData()) ;
+        }
+        By promises then
+    getData().then((resp)=>{
+    resp.find({}).toArray().then((data)=>{
+        console.log(data);
+    })
+    })
+    console.log(getData()) ;
     //Easier way Of doing same thing By async await
 
   const main = async ()=>{
